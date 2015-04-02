71db4503c626a3af1eba1e9638546facef451bef
Guilherme Mattar Bastos <guilhermembastos@gmail.com>
2015-04-02 03:00:28 -0300

    Remove authentication requirement.

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-    var db = new Firebase('https://lean-canvas.firebaseio.com/');
-        path_usr = window.location.search.slice(6).replace('/', '');
+    var db = new Firebase('https://lean-canvas.firebaseio.com/'),
+        canvas_id = window.location.search.slice(6).replace('/', '');
 
     $(document).foundation();
 
@@ -17,247 +17,247 @@ $(document).ready(function() {
         }
     });
 
-    $('#login').addClass('display-none');
-    $('#canvas').addClass('display-none');
-    $('.e-canvas-edit').addClass('display-none');
-    $('.e-canvas-login').addClass('display-none');
-    $('.e-canvas-logout').addClass('display-none');
+    if (!canvas_id) {
+        state1(db);
+
+    } else {
+        state2(db, canvas_id);
+    }
+
+    $('.b-project-list').on('click', '.e-project-list-item a', function() {
+        state2(db, $(this).text());
+    });
+
+    $('.e-add-comment').click(function() {
+        $(this).addClass('m-display-none');
+        $(this).siblings('.e-canvas-comment').removeClass('m-display-none');
+    });
+
+    $('.b-create-canvas .e-button').click(function() {
+        var id = $(this).prev().val();
+
+        db.child('canvases').child(id).once('value', function(snapshot) {
+            var doc = {},
+                url = location.pathname + '?user=';
 
 
-    db.child('users').once('value', function(snapshot) {
-        for (var obj in snapshot.val()) {
-            var $a = $('<a/>');
+            if (!snapshot.val()) {
+                doc.ux = '';
+                doc.foes = '';
+                doc.risk = '';
+                doc.costs = '';
+                doc.field = '';
+                doc.causes = '';
+                doc.events = '';
+                doc.changes = '';
+                doc.metrics = '';
+                doc.problem = '';
+                doc.adoption = '';
+                doc.approach = '';
+                doc.evidence = '';
+                doc.impacted = '';
+                doc.mechanism = '';
+                doc.partners = '';
+                doc.resources = '';
+                doc.activities = '';
+                doc.supporters = '';
+                doc.proposition = '';
 
-            $a.attr('href', window.location.pathname + '?user=' + obj);
-            $a.text(snapshot.val()[obj].canvas_name);
+                doc.ux_comments = '';
+                doc.foes_comments = '';
+                doc.risk_comments = '';
+                doc.costs_comments = '';
+                doc.field_comments = '';
+                doc.causes_comments = '';
+                doc.events_comments = '';
+                doc.changes_comments = '';
+                doc.metrics_comments = '';
+                doc.problem_comments = '';
+                doc.adoption_comments = '';
+                doc.approach_comments = '';
+                doc.evidence_comments = '';
+                doc.impacted_comments = '';
+                doc.mechanism_comments = '';
+                doc.partners_comments = '';
+                doc.resources_comments = '';
+                doc.activities_comments = '';
+                doc.supporters_comments = '';
+                doc.proposition_comments = '';
 
-            $('.b-offcanvas').append($a);
+                db.child('canvases').child(id).set(doc);
+
+                state0();
+
+                $('#canvas-name').text(id);
+                $('#edit-canvas .e-canvas-name').val(id);
+
+                history.pushState(null, null, url + id);
+
+                $('#canvas').removeClass('m-display-none');
+
+            } else {
+                notify('error', 'This canvas already exists');
+            }
+        });
+
+        return false;
+    });
+
+    $('.e-canvas-content, .e-canvas-comment').focus(function() {
+        $(this).data('prev-val', $(this).val());
+    });
+
+    $('.e-canvas-content, .e-canvas-comment').blur(function() {
+        if ($(this).data('prev-val') != $(this).val()) {
+            var doc = {};
+
+            doc[$(this).attr('name')] = $(this).val();
+
+            db.child('canvases').child($('#canvas-name').text()).update(doc);
+
+            notify('success', 'Canvas saved');
         }
     });
 
-    db.onAuth(function (auth) {
-        var usr = path_usr;
+    // $('#edit-canvas .e-button').click(function() {
+    //     var old_id = $('#canvas-name').text(),
+    //         new_id = $('#edit-canvas .e-canvas-name').val(),
+    //         url = location.pathname + '?user=' + new_id;
 
-        $('#login').addClass('display-none');
-        $('#canvas').addClass('display-none');
-        $('.e-canvas-edit').addClass('display-none');
-        $('.e-canvas-login').addClass('display-none');
-        $('.e-canvas-logout').addClass('display-none');
 
-        clear_answers();
+    //     db.child('canvases').child(new_id).once('value', function(snapshot) {
+    //         var doc = {};
 
-        if (auth || usr) {
-            if (auth) {
-                // TODO: remove this after team canvas.
-                $('.e-canvas-textarea').prop('disabled', false);
-                //
+    //         if (!snapshot.val()) {
+    //             doc.id = id;
 
-                if (!path_usr || path_usr == auth.uid) {
-                    var url = window.location.pathname + '?user=' + auth.uid;
+    //             db.child('canvases').child(old_id).update(doc);
 
-                    usr = auth.uid;
-                    path_usr = auth.uid;
+    //             $('#canvas-name').text(new_id);
 
-                    // $('.e-canvas-textarea').prop('disabled', false);
+    //             history.pushState(null, null, url);
 
-                    history.pushState(null, null, url);
+    //             $('#edit-canvas').foundation('reveal', 'close');
 
-                    $('.e-canvas-edit').removeClass('display-none');
-                }
+    //         } else {
+    //             notify('error', 'This canvas already exists');
+    //         }
+    //     });
+    // });
+});
 
-                $('.e-canvas-logout').removeClass('display-none');
+function reset_inputs() {
+    $('.e-canvas-content').val('');
+    $('.e-canvas-comment').val('').addClass('m-display-none');
+    $('.e-add-comment').removeClass('display-none');
+    $('#canvas-name').text('Canvas');
+    $('#edit-canvas .e-canvas-name').val('');
+}
 
-            } else {
-                $('.e-canvas-login').removeClass('display-none');
-            }
+function state0() {
+    $('#canvas').addClass('m-display-none');
+    $('#landing').addClass('m-display-none');
+    $('.e-canvas-edit').addClass('m-display-none');
 
-            db.child('users').child(usr).once('value', function(snapshot) {
-                function get_username() {
-                    if (auth.provider == 'google') {
-                        return auth.google.email;
+    reset_inputs();
+}
 
-                    } else if (auth.provider == 'twitter') {
-                        return auth.twitter.id;
-                    }
-                }
+function state1(db) {
+    state0();
 
-                var data = snapshot.val(),
-                    doc = {};
+    db.child('canvases').once('value', function(snapshot) {
+        $('.b-project-list p').remove();
 
-                if (data) {
-                    $('#canvas-idea').text(data.idea);
-                    $('#canvas-costs').text(data.costs);
-                    $('#canvas-risks').text(data.risks);
-                    $('#canvas-users').text(data.users);
-                    $('#canvas-impact').text(data.impact);
-                    $('#canvas-theory').text(data.theory);
-                    $('#canvas-metrics').text(data.metrics);
-                    $('#canvas-problem').text(data.problem);
-                    $('#canvas-channels').text(data.channels);
-                    $('#canvas-partners').text(data.partners);
-                    $('#canvas-solution').text(data.solution);
-                    $('#canvas-champions').text(data.champions);
-                    $('#canvas-opponents').text(data.opponents);
-                    $('#canvas-competitors').text(data.competitors);
-                    $('#canvas-sustainability').text(data.sustainability);
+        for (var obj in snapshot.val()) {
+            var $a = $('<a/>').attr('href', '#').text(obj),
+                $p = $('<p/>').addClass('e-project-list-item');
 
-                    $('#canvas-name').text(data.canvas_name);
-                    $('#edit-canvas .e-canvas-name').val(data.canvas_name);
+            $('.b-project-list').append($p.append($a));
+            $('.b-project-list').removeClass('m-display-none');
+        }
 
-                    $('#canvas').removeClass('display-none');
+        $('#landing').removeClass('m-display-none');
+    });
+}
 
-                } else if (path_usr == auth.uid) {
-                    doc.name = auth[auth.provider].displayName;
-                    doc.idea = '';
-                    doc.costs = '';
-                    doc.risks = '';
-                    doc.users = '';
-                    doc.impact = '';
-                    doc.theory = '';
-                    doc.metrics = '';
-                    doc.problem = '';
-                    doc.channels = '';
-                    doc.partners = '';
-                    doc.solution = '';
-                    doc.username = get_username(auth);
-                    doc.champions = '';
-                    doc.opponents = '';
-                    doc.competitors = '';
-                    doc.canvas_name = doc.name;
-                    doc.sustainability = '';
+function state2(db, id) {
+    state0();
 
-                    db.child('users').child(auth.uid).set(doc);
+    db.child('canvases').child(id).once('value', function(snapshot) {
+        var data = snapshot.val();
 
-                    $('#canvas-name').text(doc.canvas_name);
-                    $('#edit-canvas .e-canvas-name').val(doc.canvas_name);
+        if (data) {
+            $('#canvas-ux').val(data.ux);
+            $('#canvas-foes').val(data.foes);
+            $('#canvas-risk').val(data.risk);
+            $('#canvas-costs').val(data.costs);
+            $('#canvas-field').val(data.field);
+            $('#canvas-causes').val(data.causes);
+            $('#canvas-events').val(data.events);
+            $('#canvas-changes').val(data.changes);
+            $('#canvas-metrics').val(data.metrics);
+            $('#canvas-problem').val(data.problem);
+            $('#canvas-adoption').val(data.adoption);
+            $('#canvas-approach').val(data.approach);
+            $('#canvas-evidence').val(data.evidence);
+            $('#canvas-impacted').val(data.impacted);
+            $('#canvas-mechanism').val(data.mechanism);
+            $('#canvas-partners').val(data.partners);
+            $('#canvas-resources').val(data.resources);
+            $('#canvas-activities').val(data.activities);
+            $('#canvas-supporters').val(data.supporters);
+            $('#canvas-proposition').val(data.proposition);
 
-                    $('#canvas').removeClass('display-none');
+            $('#canvas-ux-comments').val(data.ux_comments);
+            $('#canvas-foes-comments').val(data.foes_comments);
+            $('#canvas-risk-comments').val(data.risk_comments);
+            $('#canvas-costs-comments').val(data.costs_comments);
+            $('#canvas-field-comments').val(data.field_comments);
+            $('#canvas-causes-comments').val(data.causes_comments);
+            $('#canvas-events-comments').val(data.events_comments);
+            $('#canvas-changes-comments').val(data.changes_comments);
+            $('#canvas-metrics-comments').val(data.metrics_comments);
+            $('#canvas-problem-comments').val(data.problem_comments);
+            $('#canvas-adoption-comments').val(data.adoption_comments);
+            $('#canvas-approach-comments').val(data.approach_comments);
+            $('#canvas-evidence-comments').val(data.evidence_comments);
+            $('#canvas-impacted-comments').val(data.impacted_comments);
+            $('#canvas-mechanism-comments').val(data.mechanism_comments);
+            $('#canvas-partners-comments').val(data.partners_comments);
+            $('#canvas-resources-comments').val(data.resources_comments);
+            $('#canvas-activities-comments').val(data.activities_comments);
+            $('#canvas-supporters-comments').val(data.supporters_comments);
+            $('#canvas-proposition-comments').val(data.proposition_comments);
+
+            $('#canvas-name').text(id);
+            $('#edit-canvas .e-canvas-name').val(id);
+
+            $('.e-canvas-comment.m-display-none').each(function() {
+                if ($(this).val()) {
+                    $(this).removeClass('m-display-none');
+                    $(this).siblings('i').addClass('m-display-none');
+
+                } else {
+                    $(this).siblings('i').removeClass('m-display-none');
                 }
             });
 
+            history.pushState(null, null, location.pathname + '?user=' + id);
+
+            $('#canvas').removeClass('m-display-none');
+
         } else {
-            $('#login').removeClass('display-none');
+            state1(db);
         }
     });
+}
 
-    $('.e-canvas-textarea:enabled').focus(function() {
-        var auth = db.getAuth();
+function notify(type, text) {
+    $('.b-system-message').text(text);
+    $('.b-system-message').addClass('m-visible ' + type);
 
-        // if (auth && auth.uid == path_usr) {
-        if (auth) {
-            $(this).data('prev', $(this).val());
-        }
-    });
-    $('.e-canvas-textarea:enabled').blur(function() {
-        var auth = db.getAuth();
+    setTimeout(function() {
+        $('.b-system-message').removeClass('m-visible ' + type);
 
-        // if (auth && auth.uid == path_usr) {
-        if (auth && $(this).data('prev') != $(this).val()) {
-            var doc = {};
-
-            doc[$(this).attr('id').slice(7)] = $(this).val();
-
-            db.child('users').child(path_usr).update(doc);
-
-            $('.b-system-message').addClass('m-visible');
-
-            setTimeout(function() {
-                $('.b-system-message').removeClass('m-visible');
-
-            }, 2000);
-        }
-    });
-
-    $('#edit-canvas .e-button').click(function() {
-        var auth = db.getAuth(),
-            path = window.location.pathname + '?user=',
-            data = {};
-
-        if (auth && auth.uid == path_usr) {
-            data.canvas_name = $('#edit-canvas .e-canvas-name').val();
-
-            // TODO: Ensure name is unique.
-
-            db.child('users').child(auth.uid).update(data);
-
-            $('#canvas-name').text(data.canvas_name);
-
-            // history.pushState(null, null, path + data.canvas_name);
-
-            $('#edit-canvas').foundation('reveal', 'close');
-        }
-    });
-
-    $('.google-login').click(function() {
-        db.authWithOAuthPopup('google', function(error, auth) {
-            if (error) {
-                $('#login').removeClass('display-none');
-                $('#canvas').addClass('display-none');
-                $('.e-canvas').addClass('display-none');
-                $('.e-canvas-login').addClass('display-none');
-                $('.e-canvas-logout').addClass('display-none');
-
-            } else {
-              clear_answers();
-            }
-        }, {scope: 'email'});
-    });
-
-    $('.twitter-login').click(function() {
-        db.authWithOAuthPopup('twitter', function(error, auth) {
-            if (error) {
-                $('#login').removeClass('display-none');
-                $('#canvas').addClass('display-none');
-                $('.e-canvas').addClass('display-none');
-                $('.e-canvas-login').addClass('display-none');
-                $('.e-canvas-logout').addClass('display-none');
-
-            } else {
-              clear_answers();
-            }
-        });
-    });
-
-    $('.e-canvas-logout').click(function() {
-        db.unauth();
-        clear_answers();
-
-        $('#login').removeClass('display-none');
-        $('#canvas').addClass('display-none');
-        $('.e-canvas-edit').addClass('display-none');
-        $('.e-canvas-login').addClass('display-none');
-        $('.e-canvas-logout').addClass('display-none');
-
-        history.pushState(null, null, window.location.pathname);
-    });
-
-    $('.e-canvas-login').click(function() {
-        $('#login').removeClass('display-none');
-        $('#canvas').addClass('display-none');
-        $('.e-canvas-edit').addClass('display-none');
-        $('.e-canvas-login').addClass('display-none');
-        $('.e-canvas-logout').addClass('display-none');
-
-        history.pushState(null, null, window.location.pathname);
-    });
-});
-
-function clear_answers() {
-    $('#canvas-name').text('Canvas');
-    $('#canvas-idea').text('');
-    $('#canvas-costs').text('');
-    $('#canvas-risks').text('');
-    $('#canvas-users').text('');
-    $('#canvas-impact').text('');
-    $('#canvas-theory').text('');
-    $('#canvas-metrics').text('');
-    $('#canvas-problem').text('');
-    $('#canvas-channels').text('');
-    $('#canvas-partners').text('');
-    $('#canvas-solution').text('');
-    $('#canvas-champions').text('');
-    $('#canvas-opponents').text('');
-    $('#canvas-competitors').text('');
-    $('#canvas-sustainability').text('');
-    $('#edit-canvas .e-canvas-name').val('');
+    }, 2000);
 }
