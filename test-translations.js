c3b8bb9a65f45394209beaf00e4303a0da4bf982
Atul Varma <varmaa@gmail.com>
2015-11-30 14:39:57 -0500

    better feedback for translation test.

--- test-translations.js
+++ test-translations.js
@@ -1,12 +1,29 @@
-var assert = require('assert');
-
 var translations = require('./static/js/translations.js');
 
 var allKeys = Object.keys(translations.en);
 
+var errors = 0;
+
 Object.keys(translations).forEach(function(language) {
   console.log("Testing translation", language);
-  assert.deepEqual(Object.keys(translations[language]), allKeys);
+
+  allKeys.forEach(function(key) {
+    if (!translations[language][key]) {
+      errors++;
+      console.log("Key '" + key + "' not present in " + language +
+                  " translation");
+    }
+  });
+
+  Object.keys(translations[language]).forEach(function(key) {
+    if (allKeys.indexOf(key) == -1) {
+      errors++;
+      console.log("Extra key '" + key + "' found in " + language +
+                  " translation");
+    }
+  });
 });
 
-console.log("Translations are OK!");
+console.log("Done testing.");
+
+process.exit(errors);
